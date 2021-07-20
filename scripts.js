01f7ca64434477f7841ad739fcb3662176e2e4e8
kaku <akash.sharma@rtcamp.com>
2021-07-20 18:38:44 +0530

    Desktop-xl style update v1

--- scripts.js
+++ scripts.js
@@ -6,6 +6,13 @@ tabs.forEach(el => {
 })
 
 function fetchPosts(event) {
+
+	document.querySelectorAll('.billboard__categories-tab').forEach( el => {
+		el.classList.remove('active');
+	})
+
+	event.target.classList.add('active');
+
 	fetch("./data.json")
 		.then(response => response.json())
 		.then(json => {
@@ -23,18 +30,18 @@ function updatePosts(posts) {
 	  container.removeChild(container.lastChild);
 	}
 
-	posts.map(post => {
-		const htmlString = `<div class="billboard__post">
-						<div class="billboard__post-image">
-							<img src="${post.image}" />
-						</div>
-						<div class="billboard__post-text">
-							<h2>${post.heading}</h2>
-							<h5>${post.publish}</h5>
-							<h5>${post.author}</h5>
-						</div>
-					</div>`;
+	posts.map( (post, index) => {
+		const htmlString = `<div class="billboard__post-image">
+								<img src="${post.image}" />
+							</div>
+							<div class="billboard__post-text">
+								${ index === 0 ? `<h4><span>${post.category}</span></h4>` : '' }
+								<p>${post.heading}</p>
+								<p>${post.publish}</p>
+								<p>${post.author}</p>
+							</div>`;
 		var htmlObject = document.createElement('div');
+		htmlObject.classList.add('billboard__post');
 		htmlObject.innerHTML=htmlString;
 		document.querySelector(".billboard__posts").insertAdjacentElement('beforeend', htmlObject);
 	})
